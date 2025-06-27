6a88ec71f35e8d483586383a5ecd9fe10d690f0c
Adam Albarghouthi <adambargh@gmail.com>
2025-06-27 13:55:42 -0400

    fix: change sub id

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 // TODO: Replace with your own values
 // Recommended: save values into your customer object
-export const EXTERNAL_SUBSCRIPTION_ID="28f75338-8a47-4ffa-bfc6-75b19ff8f813";
+export const EXTERNAL_SUBSCRIPTION_ID="52f30f81-726f-47b9-9543-c554fba237bd";
 export const EXTERNAL_CUSTOMER_ID="56b83f3e-8685-4279-bb57-6e028df11d21";
