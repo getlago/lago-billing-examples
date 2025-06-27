c75f56c47656c136f4acb4c93037fa80c584cc5d
Adam Albarghouthi <adambargh@gmail.com>
2025-06-27 15:57:10 -0400

    fix: get sub id from req body

--- src/app/api/(events)/seat-usage/route.ts
+++ src/app/api/(events)/seat-usage/route.ts
@@ -1,17 +1,21 @@
 import { NextResponse } from 'next/server';
 import lagoClient from '@/lib/lagoClient';
-import { EXTERNAL_SUBSCRIPTION_ID } from '@/lib/constants';
 
 export async function POST(request: Request) {
   const body = await request.json();
   const seatId = body.seat_id;
   const operationType = body.operation_type;
+  const external_subscription_id = body.external_subscription_id;
+
+  if (!external_subscription_id) {
+    return NextResponse.json({ error: 'External subscription ID is required' }, { status: 400 });
+  }
 
   try {
     const response = await lagoClient.events.createEvent({
       event: {
         transaction_id: crypto.randomUUID(),
-        external_subscription_id: EXTERNAL_SUBSCRIPTION_ID,
+        external_subscription_id,
         code: 'seats',
         timestamp: Math.floor(Date.now() / 1000).toString(),
         properties: {
