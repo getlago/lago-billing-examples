271b6052334dbd8210b2ef82e072fed7f872847d
Adam Albarghouthi <adambargh@gmail.com>
2025-06-27 15:55:05 -0400

    fix: account for hybrid plan

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { EXTERNAL_SUBSCRIPTION_ID, EXTERNAL_SUBSCRIPTION_ID_HYBRID } from "./constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 } 
 
-export async function getCurrentUsage() {
-  const response = await fetch('/api/cus-usage');
+export async function getCurrentUsage(hybrid: boolean = false) {
+  const response = await fetch(`/api/cus-usage?external_subscription_id=${hybrid ? EXTERNAL_SUBSCRIPTION_ID_HYBRID : EXTERNAL_SUBSCRIPTION_ID}`);
   const data = await response.json();
   return data;
 };
